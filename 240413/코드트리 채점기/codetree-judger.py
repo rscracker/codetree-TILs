ccef1bc583609900b9f3c1410ea754a08292d2ec
rscracker <81612555+rscracker@users.noreply.github.com>
2024-04-13 02:07:47 +0900

    Title: 코드트리 채점기, Time: 3017ms, Memory: 29MB, Status: Passed - CodeTree

--- 240413/코드트리 채점기/codetree-judger.py
+++ 240413/코드트리 채점기/codetree-judger.py
@@ -1,78 +1,94 @@
-from collections import defaultdict, deque
-import heapq
+import sys
+from collections import deque, defaultdict, Counter
+from itertools import product,combinations
+import bisect,math,heapq
 
 Q = int(input())
-N = -1
-
-checker = defaultdict(str)
-checking = defaultdict(int)
-waiting_queue = []
+N = 0
+waiting_queue = defaultdict(list)
 waiting_urls = defaultdict(list)
-logs = defaultdict(list)
+logs = defaultdict(tuple)
+checking = defaultdict(int)
+checker = defaultdict(str)
 
-def request(priority, time ,url):
-    if waiting_urls[url]: return
-    push_waiting(priority, time, url)
-
-def push_waiting(priority, time, url):
-    heapq.heappush(waiting_queue, (priority, time, url))
-    waiting_urls[url] = [priority, time]
-
-def try_checking(t):
-    if not waiting_queue: return
-    temp = deque()
-    priority, time, url = heapq.heappop(waiting_queue)
-    domain, id = url.split('/')
-    while waiting_queue and not check_possible(t, domain):
-        temp.append((priority, time, url))
-        priority, time, url = heapq.heappop(waiting_queue)
-    if check_possible(t, domain):
-        for i in range(1, N + 1):
-            if not checker[i]:
-                waiting_urls.pop(url)
-                checking[url] = t
-                checker[i] = url
-                break
-    else:
-        heapq.heappush(waiting_queue, (priority, time, url))
-    while temp:
-        task = temp.popleft()
-        heapq.heappush(waiting_queue, task)
+def request(t,p,u):
+    domain,id = u.split('/')
+    if id in waiting_urls[domain]: return
+    heapq.heappush(waiting_queue[domain], (p,t,u))
+    waiting_urls[domain].append(id)
+    return
 
 
-def check_possible(t, domain):
+def try_check(t):
+    temp = []
+    for domain in list(waiting_queue.keys()):
+        if not waiting_queue[domain]: continue
+        if isChecking(domain) and checkTime(domain, t):
+            heapq.heappush(temp, waiting_queue[domain][0])
+    if not temp: return
+    task = heapq.heappop(temp)
+    start_check(t, task)
+    return
+
+
+def isChecking(domain):
     if checking[domain]: return False
-    if logs[domain]:
-        start, gap = logs[domain]
-        if t < start + 3 * gap: return False
     return True
 
-def finish(J_id, t):
-    if not checker[J_id]: return
-    url = checker[J_id]
+def checkTime(domain, t): 
+    if not logs[domain]: return True
+    start, gap = logs[domain]
+    if t < start + 3 * gap: return False
+    return True
+    
+def start_check(t, task):
+    priority, time, url = task
     domain, id = url.split('/')
-    checker[J_id] = ''
-    start = checking[url]
+    for i in range(1, N + 1):
+        if not checker[i]:
+            checker[i] = domain
+            checking[domain] = t
+            heapq.heappop(waiting_queue[domain])
+            waiting_urls[domain].remove(id)
+            return
+
+def finish(t, J_id):
+    if not checker[J_id]: return
+    domain = checker[J_id] 
+    start = checking[domain]
     logs[domain] = (start, t - start)
-    checking.pop(url)
+    checker[J_id] = ''
+    checking.pop(domain)
+    return
 
-
+def answer():
+    cnt = 0
+    for domain in list(waiting_queue.keys()):
+        cnt += len(waiting_queue[domain])
+    print(cnt)
 
 for _ in range(Q):
     temp = list(input().split())
     inst = int(temp[0])
     if inst == 100:
-        N, u0 = int(temp[1]), temp[2]
-        push_waiting(1,0,u0)
-    if inst == 200:
-        t,p = map(int, temp[1:-1])
-        u = temp[-1]
-        request(p,t,u)
-    if inst == 300:
+        N,u0 = temp[1:]
+        domain,id = u0.split('/')
+        N = int(N)
+        heapq.heappush(waiting_queue[domain], (1,0,u0))
+        waiting_urls[domain].append(id)
+
+    elif inst == 200:
+        t,p,u = temp[1:]
+        t,p = int(t), int(p)
+        request(t,p,u)
+        
+    elif inst == 300:
         t = int(temp[1])
-        try_checking(t)
-    if inst == 400:
-        t,J_id = map(int, temp[1:])
-        finish(J_id, t)
-    if inst == 500:
-        print(len(waiting_queue))
+        try_check(t)
+        
+    elif inst == 400:
+        t, J_id = map(int, temp[1:])
+        finish(t, J_id)
+    
+    elif inst == 500:
+        answer()
